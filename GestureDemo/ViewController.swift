5971980c795a009dc315ce90873864bd5caaf0ea
Mohammad Kiani <kianimohammad@outlook.com>
2020-06-09 15:39:09 -0400

    Tap gesture and shake motion demo

--- GestureDemo/ViewController.swift
+++ GestureDemo/ViewController.swift
@@ -13,6 +13,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        view.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        view.addGestureRecognizer(swipeRight)
+    }
+    
+    @objc func swiped(gesture: UISwipeGestureRecognizer) {
+        let swipeGesture = gesture as UISwipeGestureRecognizer
+        switch swipeGesture.direction {
+        case UISwipeGestureRecognizer.Direction.left:
+            print("swipe left gesture")
+        case UISwipeGestureRecognizer.Direction.right:
+            print("swipe right gesture")
+        default:
+            break
+        }
+    }
+    
+    // shake motion
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if event?.subtype == UIEvent.EventSubtype.motionShake {
+            print("phone is shaking")
+        }
     }
 
 
